4cd9de0e73c561c0a481093c0e594fe0f42ca9f3
sivaram <siva.ram.konka@gmail.com>
2024-06-14 11:00:44 +0000

    made the changes in policy

--- iam-role.tf
+++ iam-role.tf
@@ -1,10 +1,11 @@
 resource "aws_cloudwatch_log_group" "example" {
-  count = var.vpc_flow_logs ? 1 : 0
-  name = "${local.name}-${var.log_group_name}"
+  for_each = var.vpc_flow_logs ? { "instance": 1 } : {}
+  name     = "${local.name}-${var.log_group_name}"
 }
 
 data "aws_iam_policy_document" "assume_role" {
-  count = var.vpc_flow_logs ? 1 : 0
+  for_each = var.vpc_flow_logs ? { "instance": 1 } : {}
+
   statement {
     effect = "Allow"
 
@@ -18,13 +19,14 @@ data "aws_iam_policy_document" "assume_role" {
 }
 
 resource "aws_iam_role" "example" {
-  count = var.vpc_flow_logs ? 1 : 0
+  for_each = var.vpc_flow_logs ? { "instance": 1 } : {}
   name               = "${local.name}-${var.iam_role_name}"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+  assume_role_policy = data.aws_iam_policy_document.assume_role[each.key].json
 }
 
 data "aws_iam_policy_document" "example" {
-  count = var.vpc_flow_logs ? 1 : 0
+  for_each = var.vpc_flow_logs ? { "instance": 1 } : {}
+
   statement {
     effect = "Allow"
 
@@ -41,8 +43,92 @@ data "aws_iam_policy_document" "example" {
 }
 
 resource "aws_iam_role_policy" "example" {
-  count = var.vpc_flow_logs ? 1 : 0
-  name   = "${local.name}-${var.iam_policy_name}"
-  role   = aws_iam_role.example.id
-  policy = data.aws_iam_policy_document.example.json
+  for_each = var.vpc_flow_logs ? { "instance": 1 } : {}
+  name     = "${local.name}-${var.iam_policy_name}"
+  role     = aws_iam_role.example[each.key].id
+  policy   = data.aws_iam_policy_document.example[each.key].json
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# resource "aws_cloudwatch_log_group" "example" {
+#   count = var.vpc_flow_logs ? 1 : 0
+#   name = "${local.name}-${var.log_group_name}"
+# }
+
+# data "aws_iam_policy_document" "assume_role" {
+#   count = var.vpc_flow_logs ? 1 : 0
+#   statement {
+#     effect = "Allow"
+
+#     principals {
+#       type        = "Service"
+#       identifiers = ["vpc-flow-logs.amazonaws.com"]
+#     }
+
+#     actions = ["sts:AssumeRole"]
+#   }
+# }
+
+# resource "aws_iam_role" "example" {
+#   count = var.vpc_flow_logs ? 1 : 0
+#   name               = "${local.name}-${var.iam_role_name}"
+#   assume_role_policy = data.aws_iam_policy_document.assume_role[count.index].json
+# }
+
+# data "aws_iam_policy_document" "example" {
+#   count = var.vpc_flow_logs ? 1 : 0
+#   statement {
+#     effect = "Allow"
+
+#     actions = [
+#       "logs:CreateLogGroup",
+#       "logs:CreateLogStream",
+#       "logs:PutLogEvents",
+#       "logs:DescribeLogGroups",
+#       "logs:DescribeLogStreams",
+#     ]
+
+#     resources = ["*"]
+#   }
+# }
+
+# resource "aws_iam_role_policy" "example" {
+#   count = var.vpc_flow_logs ? 1 : 0
+#   name   = "${local.name}-${var.iam_policy_name}"
+#   role   = aws_iam_role.example[count.index].id
+#   policy = data.aws_iam_policy_document.example[count.index].json
+# }
