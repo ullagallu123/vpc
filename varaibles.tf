5b75900cf0650a1535b40590dcc6e08323ed9205
sivaram <siva.ram.konka@gmail.com>
2024-06-19 07:52:43 +0000

    correcting the error

--- varaibles.tf
+++ varaibles.tf
@@ -1,89 +1,127 @@
-# enivronment vairables
+# Environment variables
 variable "environemt" {
-  default = ""
-}
-variable "project_name" {
-  default = ""
+  description = "The environment for the VPC (e.g., dev, staging, prod)"
+  default     = ""
 }
 
-# common vairables
+variable "project_name" {
+  description = "The name of the project"
+  default     = ""
+}
+
+# Common variables
 variable "common_tags" {
-  default = {}
+  description = "A map of tags to apply to all resources"
+  default     = {}
 }
+
 variable "azs" {
-  default = []
+  description = "A list of availability zones to use for the subnets"
+  default     = []
 }
-# vpc variables
+
+# VPC variables
 variable "vpc_cidr" {
-  default = ""
+  description = "The CIDR block for the VPC"
+  default     = ""
 }
+
 variable "enable_dns_hostnames" {
-  default = true
+  description = "A boolean flag to enable/disable DNS hostnames in the VPC"
+  default     = true
 }
+
 variable "enable_dns_support" {
-  default = true
+  description = "A boolean flag to enable/disable DNS support in the VPC"
+  default     = true
 }
+
 variable "vpc_tags" {
-  default = {}
+  description = "A map of tags to apply to the VPC"
+  default     = {}
 }
 
 # IGW variables
 variable "igw_tags" {
-  default = {}
+  description = "A map of tags to apply to the Internet Gateway"
+  default     = {}
 }
 
-# public subnet variables
+# Public subnet variables
 variable "public_subnet_cidrs" {
-  default = []
+  description = "A list of CIDR blocks for the public subnets"
+  default     = []
 }
+
 variable "public_subnet_tags" {
-  default = {}
+  description = "A map of tags to apply to the public subnets"
+  default     = {}
 }
+
 variable "public_rtg_tags" {
-  default = {}
+  description = "A map of tags to apply to the public route tables"
+  default     = {}
 }
 
-# private subnet variables
+# Private subnet variables
 variable "private_subnet_cidrs" {
-  default = []
-}
-variable "private_subnet_tags" {
-  default = {}
-}
-variable "private_rtg_tags" {
-  default = {}
+  description = "A list of CIDR blocks for the private subnets"
+  default     = []
 }
 
-# db subnet variables
+variable "private_subnet_tags" {
+  description = "A map of tags to apply to the private subnets"
+  default     = {}
+}
+
+variable "private_rtg_tags" {
+  description = "A map of tags to apply to the private route tables"
+  default     = {}
+}
+
+# DB subnet variables
 variable "db_subnet_cidrs" {
-  default = []
+  description = "A list of CIDR blocks for the database subnets"
+  default     = []
 }
+
 variable "db_subnet_tags" {
-  default = {}
+  description = "A map of tags to apply to the database subnets"
+  default     = {}
 }
+
 variable "db_rtg_tags" {
-  default = {}
+  description = "A map of tags to apply to the database route tables"
+  default     = {}
 }
 
 variable "db_group_tags" {
-  default = {}
+  description = "A map of tags to apply to the database subnet group"
+  default     = {}
 }
+
 variable "nat_tags" {
-  default = {}
+  description = "A map of tags to apply to the NAT Gateway"
+  default     = {}
 }
 
 variable "nat" {
-  default = false
+  description = "A boolean flag to enable/disable the creation of NAT Gateways"
+  default     = false
 }
 
+# VPC Peering variables
 variable "peering" {
-  default = false
+  description = "A boolean flag to enable/disable VPC peering"
+  default     = false
 }
 
 variable "acceptor_vpc_id" {
-  default = ""
+  description = "The ID of the acceptor VPC for the peering connection"
+  default     = ""
 }
 
 variable "vpc_peering_tags" {
-  default = {}
+  description = "A map of tags to apply to the VPC peering connection"
+  default     = {}
 }
